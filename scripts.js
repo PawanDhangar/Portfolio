c08f4e84ce779ac45835fb927fafa905aa37f63e
Pawan <pawandhangar113@gmail.com>
2023-08-23 22:23:46 +0530

    update 8

--- scripts.js
+++ scripts.js
@@ -6,6 +6,18 @@ window.addEventListener("load", () => {
   }, 1000);
 });
 
+const navLinks = document.querySelectorAll(".nav-item");
+const toggleButton = document.getElementById("navbarSupportedContent");
+const bsCollapse = new bootstrap.Collapse(toggleButton, {
+  toggle: false,
+});
+
+navLinks.forEach((l) => {
+  l.addEventListener("click", () => {
+    bsCollapse.hide();
+  });
+});
+
 $(document).ready(function () {
   $("a").on("click", function (event) {
     if (this.hash !== "") {
